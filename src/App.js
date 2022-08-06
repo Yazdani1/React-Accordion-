3212e79d646e7b0b7d80912165c0074b86ee58d6
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-06 21:53:55 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Data from "./Data";
 
-function App() {
+const App = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  const [showData, setShowData] = useState([]);
+
+  const loadData = () => {
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setShowData(result);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h5
+        style={{
+          textAlign: "center",
+          color: "white",
+          height: "100px",
+          justifyContent: "center",
+          display: "flex",
+          lineHeight: "100px",
+          backgroundColor: "orangered",
+        }}
+      >
+        React Accordion
+      </h5>
+
+      <div className="row">
+        {showData.map((item, index) => (
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12" key={index}>
+            <Data {...item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
